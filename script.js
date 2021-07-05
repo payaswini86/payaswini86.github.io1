47f25a410cf632f5abd52ff0b0dc6f5a9e8f3f6a
payaswini86 <prachijha223@gmail.com>
2021-07-06 01:01:58 +0530

    updated

---
+++ script.js
@@ -0,0 +1,19 @@
+var names=new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="jason";
+names[3]="paul";
+names[4]="jen";
+names[5]="frank";
+names[6]="larry";
+names[7]="laura";
+names[8]="jim";
+names[9]="paula";
+
+for (var i = 0; i < names.length; i++){
+	if(names[i].charAt(0)==='j'|| names[i].charAt(0)==='j'){
+	console.log("Goodbye "+ names[i])
+}
+else
+	console.log("hello "+ names[i])
+}
